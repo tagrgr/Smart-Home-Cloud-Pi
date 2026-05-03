73ce0a79f24b8197f0c3b7bdba6b49bee5865303
Tiago de Gregori <tiago.gregori@live.com>
2026-05-03 15:02:57 +0100

    Add file listing and download feature
    change the storage location from my sd card to a external hd drive as it has much bigger space

--- src/app.py
+++ src/app.py
@@ -1,14 +1,18 @@
-# This creates a basic flask web server for the smart home pi
-# The server allows users to upload files and stores them in the project storage folder.
+# This creates a basic flask web server for the smart home pi, the server allows users to upload files and stores them in the project storage folder.
+# Features:
+# - upload files
+# - list stored files
+# - download files
+
 
 import os
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 
 # Create flask application instance
 app = Flask(__name__)
 
 # Define where uploaded files will be saved
-UPLOAD_FOLDER = "storage"
+UPLOAD_FOLDER = "/mnt/hdd"
 
 # Create the storage fodler if it does not already exist
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
@@ -16,15 +20,27 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 # Define route for homepage
 @app.route("/")
 def home():
-    # Return a simple HTML page with a file upload form
+    # Get list of files from storage
+    files = os.listdir(UPLOAD_FOLDER)
+
+    # Build HTML page with a list of files
+    file_list_html = ""
+    for file in files:
+        file_list_html += f'<li><a href="/download/{file}">{file}</a></li>'
+
     return """
     <h1>Smart Home Cloud Pi</h1>
-    <p>Upload a file to your Raspberry Pi cloud storage.</p>
+    <h2>Upload File</h2>
 
     <form action="/upload" method="post" enctype="multipart/form-data">
         <input type="file" name="file">
         <button type="submit">Upload</button>
     </form>
+
+    <h2>Stored Files</h2>
+    <ul>
+        {file_list_html}
+    </ul>
     """
 
 # Upload route
@@ -42,11 +58,14 @@ def upload_file():
 
     # Create the full save path inside the storage folder
     save_path = os.path.join(UPLOAD_FOLDER, file.filename)
-
     # Save the uploaded file to the Raspberry Pi
     file.save(save_path)
 
-    return f"File '{file.filename}' uploaded successfully"
+    return f"File '{file.filename}' uploaded successfully <br><a href='/'>Back</a>"
+
+@app.route("/download/<filename>")
+def download_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 # Run the Flask app
 if __name__ == "__main__":
